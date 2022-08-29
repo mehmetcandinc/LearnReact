2ab6640a187b417f31bc9f4f28abfa1952e7c4bc
mehmetcandinc <dincmehmetcann@gmail.com>
2022-08-29 15:34:00 +0300

    index.js foundation laid.

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,3 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className='App'>
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'))
