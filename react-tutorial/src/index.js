a663d1ff3c6adebd5055c438505acdcabddc1c7f
mehmetcandinc <dincmehmetcann@gmail.com>
2022-08-29 15:59:49 +0300

    index.js some write examples

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -14,3 +14,11 @@ class App extends React.Component{
 }
 
 ReactDOM.render(<App/>,document.getElementById('root'))
+//*********** same output *******************
+const heading = <h1 className="site-heading">Hello, React. .</h1>
+
+const heading2 = React.createElement('h1', {className: 'site-heading'},'Hello React. . .')
+//*********** same output *******************
+
+const name = 'Can'
+const heading3 = <h1>Hello, {name}</h1>
