3aa626b26842da02a1ca8248cc4734bb0b57b364
mehmetcandinc <dincmehmetcann@gmail.com>
2022-08-29 16:41:37 +0300

    .

---
+++ react-tutorial/src/someExample.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+
+//*********** same output *******************
+const heading = <h1 className="site-heading">Hello, React. .</h1>
+
+const heading2 = React.createElement('h1', {className: 'site-heading'},'Hello React. . .')
+//*********** same output *******************
+
+const name = 'Can'
+const heading3 = <h1>Hello, {name}</h1>
+
+
+export {heading3,heading,heading2}
