615bedecca9e89e3e276ebd998d3eeff2c525ee7
mehmetcandinc <dincmehmetcann@gmail.com>
2022-08-29 16:10:32 +0300

    create App.js and index.js some contents moved

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,17 +1,8 @@
 import React,{ Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import App from 'App.js'
 
-
-class App extends React.Component{
-  render(){
-    return(
-      <div className='App'>
-        <h1>Hello React!</h1>
-      </div>
-    )
-  }
-}
 
 ReactDOM.render(<App/>,document.getElementById('root'))
 //*********** same output *******************
