727691545e61abc6990ecd51420c5b590291aac0
mehmetcandinc <dincmehmetcann@gmail.com>
2022-08-29 16:40:16 +0300

    examples moved someExsample.js

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import { heading,heading2,heading3 } from "./someExample";
+import Table from './Table'
 
 class App extends Component {
     render() {
         return(
-            <div className="App">
-                <h1>Hello React Again!</h1>
+            <div className="container">
+                <Table />
+                {heading}
+                {heading2}
+                {heading3}
             </div>
         )
     }
--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,15 +1,8 @@
 import React,{ Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from 'App.js'
+import App from './App'
+import './someExample'
 
 
 ReactDOM.render(<App/>,document.getElementById('root'))
-//*********** same output *******************
-const heading = <h1 className="site-heading">Hello, React. .</h1>
-
-const heading2 = React.createElement('h1', {className: 'site-heading'},'Hello React. . .')
-//*********** same output *******************
-
-const name = 'Can'
-const heading3 = <h1>Hello, {name}</h1>
