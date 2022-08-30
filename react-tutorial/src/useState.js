4305dbff5ce66e2844580a01f7b865866efa5309
mehmetcandinc <dincmehmetcann@gmail.com>
2022-08-30 17:06:15 +0300

    Example for useState

--- react-tutorial/src/useState.js
+++ react-tutorial/src/useState.js
@@ -32,4 +32,59 @@ const FavoriteColor = () => {
   );
 };
 
-export { Example, FavoriteColor };
+const BasicOparations = () => {
+  const [sum, setSum] = useState(0);
+  const [sub, setSub] = useState(0);
+  const [multi, setMulti] = useState(0);
+  const [div, setDiv] = useState(0);
+  const func = React.useCallback((a, b) => {
+    setSum(a + b);
+  }, []);
+const array =  [1,2,3,4,5]
+  React.useEffect(() => {
+    func(5,6)
+    return () => {};
+  }, []);
+
+  return (
+    <>
+      <h1>Basic Oparations</h1>
+
+      <input type='text' defaultValue={sum} onChange={(e)=>{setSum(e.currentTarget.value)}} />
+      <h2>
+        {" "}
+        Addition {sum}
+        <br></br> Subtraction {sub}
+        <br></br> Multiplication {multi}
+        <br></br> Division {div}{" "}
+      </h2>
+      <button
+        type="button"
+        onClick={(event) => {
+          func(Math.random(), 3);
+        }}
+      >
+        Addition
+      </button>
+      <button type="button" onClick={() => setSub(9 - 5)}>
+        Subtraction
+      </button>
+
+      <select>
+        {array.map((e) => {
+          return <option  value={e}>{e}</option>;
+        })}
+        <option></option>
+        <option></option>
+      </select>
+      <button type="button" onClick={() => setMulti(10 * 5)}>
+        Multiplication
+      </button>
+      <button type="button" onClick={() => setDiv(10 / 5)}>
+        Division
+      </button>
+    </>
+  );
+};
+
+export { Example, FavoriteColor, BasicOparations };
